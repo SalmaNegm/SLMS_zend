5f04b7c76a925ff7180104d0063865fbd4a446ac
Fatmasarhan <fatmasar7an@gmail.com>
2016-05-10 20:40:14 +0200

    Add action of requests

--- application/controllers/RequestsController.php
+++ application/controllers/RequestsController.php
@@ -14,7 +14,14 @@ class RequestsController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        $this->view->requests = $this->model->listRequests();
+    	$requests = $this->model->listRequests();
+    	foreach ($requests as  $value) 
+    	{
+    		$user_id= $value['user_id'];
+    	}
+    	
+    	$this->view->user_id=$user_id; 
+        $this->view->requests = $requests;
     }
 
     public function addAction()
@@ -35,8 +42,52 @@ class RequestsController extends Zend_Controller_Action
 
     }
 
+    public function deleteAction()
+    {
+        $id = $this->getRequest()->getParam('id');
+		if($this->model->deleteRrquest($id))
+		$this->redirect('requests/index');
+    }
+
+    public function editAction()
+    {
+        $id = $this->getRequest()->getParam('id');
+		$request = $this->model->getRequestById($id);
+		$form = new Application_Form_Request();
+		$form->populate($request[0]);
+        
+		if($this->getRequest()->isPost())
+		{
+			if($form->isValid($this->getRequest()->getParams()))
+			{
+				$data = $form->getValues();
+				$this->model->editRequest($id,$data);
+				$this->redirect('requests/index');	
+			}
+    
+    	}
+    	
+    	$this->view->form = $form;
+
+    }
+
+    public function markAction()
+    {
+        $id = $this->getRequest()->getParam('id');
+        $request = $this->model->getRequestById($id);
+        $mark = $request[0]['is_read'];
+        $this->model->markRequest($id,$mark);
+        $this->redirect('requests/index');	
+    }
+
 
 }
+
+
+
+
+
+
 
 
 
--- application/models/DbTable/Request.php
+++ application/models/DbTable/Request.php
@@ -43,6 +43,33 @@ class Application_Model_DbTable_Request extends Zend_Db_Table_Abstract
 		return $row->save();
 	}
 
+	function deleteRrquest($id)
+	{
+		return $this->delete('id='.$id);
+	}
+
+	function editRequest($id,$data)
+	{
+		return $this->update($data,"id=$id");
+	}
+
+	function markRequest($id,$mark)
+	{
+		
+		if($mark == 0)
+		{
+			$mark = 1;
+		}
+		elseif($mark == 1)
+		{
+			$mark = 0;
+		}
+
+		$data = array('is_read'  => $mark);
+
+		return $this->update($data,"id=$id");
+	}
+
 
 }
 
