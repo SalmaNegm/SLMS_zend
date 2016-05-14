2982217a192f4a8e4dc1fa0fdc2d17029d1e8562
SalmaNegm <salma-negm@outlook.com>
2016-05-14 19:34:24 +0200

    css and js files are linked from the bootstrap.php

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,6 +2,12 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+	protected function _initRequest() {
+            $this->bootstrap('FrontController');
+            $front = $this->getResource('FrontController');
+            $request = new Zend_Controller_Request_Http();
+            $front->setRequest($request);
+        }
 
 	protected function _initPlaceholders()
 	{
@@ -15,47 +21,52 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 		// Set the initial title and separator:
 		$view->headTitle('SLMS')->setSeparator(' :: ');
 		// Set the initial stylesheet:
-		// $view->headLink()->prependStylesheet("http://localhost/Zend/SLMS_project/public/css/bootstrap.css");
-		// $view->headLink()->prependStylesheet("http://localhost/Zend/SLMS_project/public/css/sb-admin.css"); //admin
-  //   	$view->headLink()->prependStylesheet("http://localhost/Zend/SLMS_project/public/css/morris.css");//admin
-  //   	$view->headLink()->prependStylesheet("http://localhost/Zend/SLMS_project/public/css/font-awesome.min.css");//admin
+		
 
-
-  //   	$view->headLink()->prependStylesheet("http://localhost/Zend/SLMS_project/public/css/font-awesome.css");//client
-  //   	$view->headLink()->prependStylesheet("http://localhost/Zend/SLMS_project/public/css/templatemo_style.css");//client
-  //   	$view->headLink()->prependStylesheet("http://localhost/Zend/SLMS_project/public/css/templatemo_misc.css");//client
-  //   	$view->headLink()->prependStylesheet("http://localhost/Zend/SLMS_project/public/css/flexslider.css");//client
-  //   	$view->headLink()->prependStylesheet("http://localhost/Zend/SLMS_project/public/css/testimonails-slider.css");//client
-  //   	$view->headLink()->prependStylesheet("http://localhost/Zend/SLMS_project/public/css/font-awesome.min.css");//client
-
-		// // Set the initial JS to load:
-		// $view->headScript()->prependFile('http://localhost/Zend/SLMS_project/public/js/jquery.js');
-		// $view->headScript()->prependFile('http://localhost/Zend/SLMS_project/public/js/bootstrap.js');
+    	$view->headLink()->prependStylesheet($view->baseUrl()."/css/font-awesome.css");//client
+    	$view->headLink()->prependStylesheet($view->baseUrl()."/css/templatemo_style.css");//client
+    	$view->headLink()->prependStylesheet($view->baseUrl()."/css/templatemo_misc.css");//client
+    	$view->headLink()->prependStylesheet($view->baseUrl()."/css/flexslider.css");//client
+    	$view->headLink()->prependStylesheet($view->baseUrl()."/css/testimonails-slider.css");//client
+    	$view->headLink()->prependStylesheet($view->baseUrl()."/css/font-awesome.min.css");//client
 
 
 
-		// $view->headScript()->prependFile('http://localhost/Zend/SLMS_project/public/js/modernizr-2.6.1-respond-1.1.0.min.js');//client
-		// $view->headScript()->prependFile('http://localhost/Zend/SLMS_project/public/js/jquery-1.11.0.min.js');//client
-		// $view->headScript()->prependFile('http://localhost/Zend/SLMS_project/public/js/jquery.gmap3.min.js');//client
-		// $view->headScript()->prependFile('http://localhost/Zend/SLMS_project/public/js/plugins.js');//client
-		// $view->headScript()->prependFile('http://localhost/Zend/SLMS_project/public/js/main.js');//client
+    	$view->headLink()->prependStylesheet($view->baseUrl()."/css/bootstrap.css");
+		$view->headLink()->prependStylesheet($view->baseUrl()."/css/sb-admin.css"); //admin
+    	$view->headLink()->prependStylesheet($view->baseUrl()."/css/morris.css");//admin
+    	$view->headLink()->prependStylesheet($view->baseUrl()."/css/font-awesome.min.css");//admin
 
 
 
-		// $view->headScript()->prependFile('http://localhost/Zend/SLMS_project/public/js/raphael.min.js');//admin
-		// $view->headScript()->prependFile('http://localhost/Zend/SLMS_project/public/js/morris.min.js');//admin
-		// $view->headScript()->prependFile('http://localhost/Zend/SLMS_project/public/js/morris-data.js');//admin
-
-		$view->headLink()->prependStylesheet('/Zend/SLMS_project/public/css/bootstrap.min.css');
 		// Set the initial JS to load:
-		$view->headScript()->prependFile('/Zend/SLMS_project/public/js/bootstrap.min.js');
-		$view->headScript()->prependFile('/Zend/SLMS_project/public/js/bootstrap.js');
+		$view->headScript()->prependFile($view->baseUrl().'/js/jquery.js');
+		$view->headScript()->prependFile($view->baseUrl().'/js/bootstrap.js');
 
 
-		$view->headScript()->prependFile('/Zend/SLMS_project/public/js/jquery-1.12.0.min.js');
-		$view->headScript()->prependFile('/Zend/SLMS_project/public/js/jquery-1.11.3.min.js');
-		$view->headScript()->prependFile('/Zend/SLMS_project/public/js/jquery-1.11.0.min.js');
-		// $view->headScript()->prependFile('/SLMS_zend/public/js/slider.js');
+
+		$view->headScript()->prependFile($view->baseUrl().'/js/modernizr-2.6.1-respond-1.1.0.min.js');//client
+		$view->headScript()->prependFile($view->baseUrl().'/js/jquery-1.11.0.min.js');//client
+		$view->headScript()->prependFile($view->baseUrl().'/js/jquery.gmap3.min.js');//client
+		$view->headScript()->prependFile($view->baseUrl().'/js/plugins.js');//client
+		$view->headScript()->prependFile($view->baseUrl().'/js/main.js');//client
+
+
+
+		$view->headScript()->prependFile($view->baseUrl().'/js/raphael.min.js');//admin
+		$view->headScript()->prependFile($view->baseUrl().'/js/morris.min.js');//admin
+		$view->headScript()->prependFile($view->baseUrl().'/js/morris-data.js');//admin
+
+		$view->headLink()->prependStylesheet($view->baseUrl().'/css/bootstrap.min.css');
+		// Set the initial JS to load:
+		$view->headScript()->prependFile($view->baseUrl().'/js/bootstrap.min.js');
+		$view->headScript()->prependFile($view->baseUrl().'/js/bootstrap.js');
+
+
+		$view->headScript()->prependFile($view->baseUrl().'/js/jquery-1.12.0.min.js');
+		$view->headScript()->prependFile($view->baseUrl().'/js/jquery-1.11.3.min.js');
+		$view->headScript()->prependFile($view->baseUrl().'/js/jquery-1.11.0.min.js');
+		// $view->headScript()->prependFile($view->baseUrl().'/js/slider.js');
    	
 	 }
 
--- application/forms/Category.php
+++ application/forms/Category.php
@@ -8,12 +8,12 @@ class Application_Form_Category extends Zend_Form
         $name = new Zend_Form_Element_Text('name');
         $name->setRequired();
         $name->setLabel('name');
-        $name->setAttrib('class','form-control col-1g-5');
+        $name->setAttrib('class','form-control col-1g-2');
 
         $description = new Zend_Form_Element_Textarea('description');
         $description->setRequired();
         $description->setLabel('description');
-        $description->setAttribs(array('class'=>'form-control','rows'=>'5'));
+        $description->setAttribs(array('class'=>'form-control col-1g-2','rows'=>'5'));
 
         $submit = new Zend_Form_Element_Submit('submit');
         $submit->setLabel('ADD');
