6163310822a2d9242b08b6f1a75939e35df3016a
YasminNaser <yasminty.91@gmail.com>
2016-05-14 19:11:58 +0300

    edit login

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,8 +3,18 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+
+	protected function _initSession(){
+		Zend_Session::start();
+		$session = new Zend_Session_Namespace( 'Zend_Auth' );
+		$session->setExpirationSeconds( 1800 );
+	}
+
 	protected function _initPlaceholders()
 	{
+
+
+
 		$this->bootstrap('View');
 		$view = $this->getResource('View');
 		$view->doctype('XHTML1_STRICT');
